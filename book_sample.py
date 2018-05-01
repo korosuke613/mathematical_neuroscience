6af12f24a6c313a75115ac65d78f25964eb1ec18
Futa Hirakoba <korosuke613613@gmail.com>
2018-05-01 16:12:08 +0900

    とりあえず作ってみたやーつ

---
+++ book_sample.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def identity_function(x):
+    return x
+
+
+def sigmoid(x):
+    """シグモイド関数"""
+    return 1 / (1 + np.exp(-x))
+
+
+def main():
+    X = np.array([1.0, 0.5])
+
+    W1 = np.array([
+        [0.1, 0.3, 0.5],
+        [0.2, 0.4, 0.6]
+    ])
+    B1 = np.array([0.1, 0.2, 0.3])
+    A1 = np.dot(X, W1) + B1
+    Z1 = sigmoid(A1)
+
+    W2 = np.array([
+        [0.1, 0.4],
+        [0.2, 0.5],
+        [0.3, 0.6]
+    ])
+    B2 = np.array([0.1, 0.2])
+    A2 = np.dot(Z1, W2) + B2
+    Z2 = sigmoid(A2)
+
+    W3 = np.array([
+        [0.1, 0.3],
+        [0.2, 0.4]
+    ])
+    B3 = np.array([0.1, 0.2])
+    A3 = np.dot(Z2, W3) + B3
+    Y = identity_function(A3)
+
+    print(Y)
+
+
+if __name__ == '__main__':
+    main()
--- mirror.py
+++ mirror.py
@@ -15,51 +15,56 @@ class Layer(ABC):
     def __init__(self):
         self.neuron = 0.0
         self.b = 0.0
-        self.w = 0.0
+        self.weight = 0.0
 
 
 class Network:
     def __init__(self, input_array_):
-        self._i_layer = InputLayer(input_array_)
-        self._h_layer = HiddenLayer()
-        self._o_layer = OutputLayer()
-        self.learning_rate = 0.005
+        self.i_layer = InputLayer(input_array_)
+        self.h_layer = HiddenLayer()
+        self.o_layer = OutputLayer()
+        self.learning_rate = 0.05
         self.delta = list()
 
     def check(self, binary):
-        self._i_layer.neuron = np.array([float(binary[0]),
-                                         float(binary[1]),
-                                         float(binary[2]),
-                                         float(binary[3]),
-                                         float(binary[4]),
-                                         float(binary[5])
+        self.i_layer.neuron = np.array([float(binary[0]),
+                                        float(binary[1]),
+                                        float(binary[2]),
+                                        float(binary[3]),
+                                        float(binary[4]),
+                                        float(binary[5])
                                         ])
         y, _ = self.forward()
         print(f"ミラーの確立は{y}です")
 
-    def forward(self):
-        W1 = self._i_layer.w
-        W2 = self._h_layer.w
-        X = self._i_layer.neuron
+    def get_delta(self):
+        total = sum(self.delta)
+        self.delta = list()
+        return total
 
-        U1 = X.dot(W1)
+    def forward(self):
+        W1 = self.i_layer.weight
+        W2 = self.h_layer.weight
+        X = self.i_layer.neuron
+
+        U1 = X.dot(W1) + self.i_layer.b
         Z1 = sigmoid(U1)
-        U2 = Z1.dot(W2)
+        U2 = Z1.T.dot(W2.T) + self.h_layer.b
         y = sigmoid(U2)
 
-        self._h_layer.neuron = Z1
-        self._o_layer.neuron = y
+        self.h_layer.neuron = Z1
+        self.o_layer.neuron = y
 
         return y, Z1
 
     def back_propagation(self):
-        W1 = self._i_layer.w
-        W2 = self._h_layer.w
-        y = self._o_layer.neuron
-        Z1 = self._h_layer.neuron
-        X = self._i_layer.neuron
+        W1 = self.i_layer.weight
+        W2 = self.h_layer.weight
+        y = self.o_layer.neuron
+        Z1 = self.h_layer.neuron
+        X = self.i_layer.neuron
 
-        if self._i_layer.correct is True:
+        if self.i_layer.correct is True:
             d = 1.0
         else:
             d = 0.0
@@ -67,40 +72,31 @@ class Network:
         delta2 = y - d
 
         self.delta.append(delta2**2)
-        print(delta2**2)
 
-        delta2 = np.array([delta2])
-        Z1 = np.array([Z1])
-        grad_W2 = Z1.T.dot(delta2)
+        grad_W2 = Z1.dot(delta2)
 
         sigmoid_dash = Z1 * (1 - Z1)
 
-        delta1 = delta2.dot(W2.T) * sigmoid_dash
+        delta1 = W2.T.dot(delta2.T) * sigmoid_dash
         X = np.array([X])
         grad_W1 = X.T.dot(delta1)
 
-        W2 -= self.learning_rate * grad_W2
+        W2 -= self.learning_rate * grad_W2.T
         W1 -= self.learning_rate * grad_W1
 
 
 class InputLayer(Layer):
     def __init__(self, input_array: dict):
         super().__init__()
-        self.correct = np.array([input_array["is_mirror"]])
+        self.correct = input_array["is_mirror"]
         self.x1 = Perceptron(float(input_array["binary"][0]))
         self.x2 = Perceptron(float(input_array["binary"][1]))
         self.x3 = Perceptron(float(input_array["binary"][2]))
         self.x4 = Perceptron(float(input_array["binary"][3]))
         self.x5 = Perceptron(float(input_array["binary"][4]))
         self.x6 = Perceptron(float(input_array["binary"][5]))
-        self.b = np.array([[0.1, 0.2]])
-        self.w = 0.01 * np.random.randn(6, 2)
-        #self.w = np.array([[0.1, 0.3],
-        #                   [0.2, 0.4],
-        #                   [0.5, 0.4],
-        #                   [0.1, 0.1],
-        #                   [0.3, 0.4],
-        #                   [0.2, 0.2]])
+        self.b = 0.01 * np.random.randn(1, 2)
+        self.weight = 0.01 * np.random.randn(6, 2)
         self.neuron = np.array([self.x1.param,
                                 self.x2.param,
                                 self.x3.param,
@@ -110,7 +106,7 @@ class InputLayer(Layer):
                                 ])
 
     def set_array(self, input_array: dict):
-        self.correct = np.array([input_array["is_mirror"]])
+        self.correct = input_array["is_mirror"]
         self.x1 = Perceptron(float(input_array["binary"][0]))
         self.x2 = Perceptron(float(input_array["binary"][1]))
         self.x3 = Perceptron(float(input_array["binary"][2]))
@@ -132,7 +128,7 @@ class HiddenLayer(Layer):
         self.u1 = Perceptron(0.0)
         self.u2 = Perceptron(0.0)
         self.b = np.array([[0.2, 0.2]])
-        self.w = 0.01 * np.random.randn(2, 1)
+        self.weight = 0.01 * np.random.randn(2, 1)
         #self.w = np.array([[0.1, 0.2]])
 
 
@@ -150,14 +146,12 @@ def main():
     #init_param()
 
     new = Network(mirror[0])
-    new.forward()
-    new.back_propagation()
-    for epoch in range(1000):
+    for epoch in range(0, 1000):
         # 1. 回路に入力を与える
-        new._i_layer.set_array(mirror[epoch % 64])
+        new.i_layer.set_array(mirror[epoch % 64])
         new.forward()
         new.back_propagation()
-    new.check("010010")
+        print(f"epoch:{epoch}, loss:{new.get_delta()}")
 
 
 def init_param():
